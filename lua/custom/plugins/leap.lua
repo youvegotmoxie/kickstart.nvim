48c7233e97fd07bd9229cbf10ec78f60ce060349
MikeB <youvegotmoxie@gmail.com>
2025-12-20 09:27:59 -0900

    fix leap global visual search

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -5,11 +5,8 @@ return {
   },
   vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap)', { desc = 'Leap [S]earch' }),
   vim.keymap.set({ 'n', 'o' }, 'gS', function()
-    require('leap.remote').action {
-      -- Automatically enter Visual mode when coming from Normal.
-      input = vim.fn.mode(true):match 'o' and '' or 'v',
-    }
-  end, { desc = 'Leap [S]earch Other Windows in Visual Mode' }),
+    require('leap.remote').action { input = 'V' }
+  end, { desc = 'Leap [S]earch All Windows in Visual Mode' }),
 
   vim.keymap.set({ 'x', 'o' }, 'R', function()
     require('leap.treesitter').select {
