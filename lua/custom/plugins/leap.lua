ec090988930354b07db39ca70de4fd5ab7a3ec63
Michael Beasley <michael.beasley@alvaria.com>
2026-01-26 11:41:39 -0900

    fix url to leap.nvim - switched to codeberg

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,6 +1,7 @@
 return {
   {
     'ggandor/leap.nvim',
+    url = 'https://codeberg.org/andyg/leap.nvim',
     dependencies = {},
   },
   vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap)', { desc = 'Leap [S]earch' }),
