60052216a00e60766e86146783b3632f1e6f5244
Michael Beasley <michael.beasley@alvaria.com>
2026-01-28 08:44:57 -0900

    remove dead ansible.nvim plugin and setup the ansible yaml ft and autocmds manually

--- lua/custom/plugins/filetype.lua
+++ lua/custom/plugins/filetype.lua
@@ -1,12 +1,47 @@
 return {
   'towolf/vim-helm',
   lazy = false,
-  dependencies = {
-    'crakkhead/ansible.nvim',
-  },
+  dependencies = {},
   vim.filetype.add {
     pattern = {
       ['.*/.github/workflows/.*'] = 'ghaction',
+      ['.*/defaults/.*%.ya?ml'] = 'yaml.ansible',
+      ['.*/host_vars/.*%.ya?ml'] = 'yaml.ansible',
+      ['.*/group_vars/.*%.ya?ml'] = 'yaml.ansible',
+      ['.*/group_vars/.*/.*%.ya?ml'] = 'yaml.ansible',
+      ['.*/playbook.*%.ya?ml'] = 'yaml.ansible',
+      ['.*/playbooks/.*%.ya?ml'] = 'yaml.ansible',
+      ['.*/roles/.*/tasks/.*%.ya?ml'] = 'yaml.ansible',
+      ['.*/roles/.*/handlers/.*%.ya?ml'] = 'yaml.ansible',
+      ['.*/tasks/.*%.ya?ml'] = 'yaml.ansible',
+      ['.*/molecule/.*%.ya?ml'] = 'yaml.ansible',
     },
   },
+  vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+    pattern = {
+      '*/defaults/*.yml',
+      '*/defaults/*.yaml',
+      '*/host_vars/*.yml',
+      '*/host_vars/*.yaml',
+      '*/group_vars/*.yml',
+      '*/group_vars/*.yaml',
+      '*/group_vars/*/*.yml',
+      '*/group_vars/*/*.yaml',
+      '*/playbook*.yml',
+      '*/playbook*.yaml',
+      '*/playbooks/*.yml',
+      '*/playbooks/*.yaml',
+      '*/roles/*/tasks/*.yml',
+      '*/roles/*/tasks/*.yaml',
+      '*/roles/*/handlers/*.yml',
+      '*/roles/*/handlers/*.yaml',
+      '*/tasks/*.yml',
+      '*/tasks/*.yaml',
+      '*/molecule/*.yml',
+      '*/molecule/*.yaml',
+    },
+    callback = function()
+      vim.bo.filetype = 'yaml.ansible'
+    end,
+  }),
 }
