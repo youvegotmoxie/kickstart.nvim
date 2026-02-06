919c4e6f406ec9bfb0e739d7ca6ee1e57c63e45b
Michael Beasley <youvegotmoxie@gmail.com>
2026-02-06 02:11:49 -0900

    add telescope search for TODO comments

--- init.lua
+++ init.lua
@@ -452,6 +452,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>sT', '<CMD>TodoTelescope<CR>', { desc = '[S]earch [T]odo' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>Z', require('telescope').extensions.zoxide.list, { desc = 'Open [Z]oxide' })
 
